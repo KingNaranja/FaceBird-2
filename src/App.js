c9c14796dddd819dfddec934f763d2af8b1114c1
KingNaranja <kledaa3@gmail.com>
2019-02-15 04:23:27 -0500

    add route path for Feed component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import './App.scss'
 import { Route, Link } from 'react-router-dom'
 
+// user auth routes
 import AuthenticatedRoute from './auth/components/AuthenticatedRoute'
 import Header from './header/Header'
 import SignUp from './auth/components/SignUp'
 import SignIn from './auth/components/SignIn'
 import SignOut from './auth/components/SignOut'
 import ChangePassword from './auth/components/ChangePassword'
+
+// feed Component
+import Feed from './feed/components/feed'
 
 class App extends Component {
   constructor () {
@@ -23,6 +27,9 @@ class App extends Component {
   setUser = user => this.setState({ user })
 
   clearUser = () => this.setState({ user: null })
+  
+  // getter method for App component's user state
+  getUser = () => this.state.user
 
   flash = (message, type) => {
     this.setState({ flashMessage: message, flashType: type })
@@ -42,11 +49,16 @@ class App extends Component {
         {flashMessage && <h3 className={flashType}>{flashMessage}</h3>}
         
         <main className="container">
+          {/* Routes */}
+          {/* <Route exact path='/' component={Home}/> */}
           <Route path='/sign-up' render={() => (
             <SignUp flash={this.flash} setUser={this.setUser} />
           )} />
           <Route path='/sign-in' render={() => (
             <SignIn flash={this.flash} setUser={this.setUser} />
+          )} />
+          <Route path='/feed' render={() => (
+            <Feed flash={this.flash} getUser={this.getUser} setUser={this.setUser} user={user} />
           )} />
           <AuthenticatedRoute user={user} path='/sign-out' render={() => (
             <SignOut flash={this.flash} clearUser={this.clearUser} user={user} />
