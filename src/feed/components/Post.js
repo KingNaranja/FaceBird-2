a872314427c43b9e6b828b4837403dae9ad5552b
KingNaranja <kledaa3@gmail.com>
2019-02-17 02:33:36 -0500

    Created stateless `Post` component
    Added Post.js, containing a function that returns a React component for
    FaceBird posts.
    
    - The component takes a post API object as a prop
    - expects the date, nickname, and text objects as props

---
+++ src/feed/components/Post.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+const Post = props => {
+// takes FaceBird post object as prop 
+// and renders Post
+
+  return (
+    <div>
+      <div className="props.nick">
+        <h4>{ props.nickname}</h4>
+      </div>
+      <div className="post-body">
+        <p>{ props.date }</p>
+        <h5>{ props.text }</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Post
