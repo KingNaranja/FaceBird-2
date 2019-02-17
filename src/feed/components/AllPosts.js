9e855387c0bacc9895e0081c8b67681679631e18
KingNaranja <kledaa3@gmail.com>
2019-02-17 03:03:57 -0500

    Add user to Feed state, and create AllPosts component

---
+++ src/feed/components/AllPosts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import Post from './Post'
+
+export class AllPosts extends Component {
+  render() {
+    return (
+      <div>
+        <Post/>
+      </div>
+    )
+  }
+}
+
+export default AllPosts
--- src/feed/components/feed.js
+++ src/feed/components/feed.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
+import RecentPost from './RecentPost'
+import AllPosts from './AllPosts'
+
 
 
 class Feed extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      user: props.getUser()
+    }
+    console.log(this.state)
   }
 
-
   render() {
+    const {getUser, setUser, user} = this.props
+
     return (
-      <div>
-        
+      <div> 
+        <RecentPost  getUser={this.props.getUser} user={this.state.user} />
+        {/* <AllPosts getUser setUser user={this.state}/> */}
       </div>
     )
   }
