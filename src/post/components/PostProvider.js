b211f9e200fd6a94bce8fe1d72269e9f32a9699b
KingNaranja <kledaa3@gmail.com>
2019-02-25 12:21:18 -0500

    Create `editPost` method for updating the posts state
    Edit Posts finds the index of the old post in the state,
    creates a copy of the post state and updates the copy. Then the PostProvider
    state is set with the new copy to update the feed for all child components.

--- src/post/components/PostProvider.js
+++ src/post/components/PostProvider.js
@@ -10,7 +10,7 @@ export class PostProvider extends Component {
       posts: []
     }
   }
-
+  // adds a post to the the state
   addPosts = posts => {
     // remove duplicate keys if any 
     this.setState( prevState => ({
@@ -18,13 +18,37 @@ export class PostProvider extends Component {
     }))
    
   }
-
+  // finds the post and remove it from state
   removePost = text => {
-    // finds the post by text and remove from state
+    
     this.setState( prevState => ({
       posts: prevState.posts.filter(post => post.text != text)
     }))
     
+  }
+
+  // update the state of a post
+  editPost = (id, post) => {
+    const { posts } = this.state
+    const { user } = this.props
+
+    // finds the index of the old post within current state
+    const postIndex = posts.findIndex( post => post._id == id ) 
+    // grab post and owner ID from patch response
+    // and create new a  updated post object
+    const { post:newPost, post:{ owner:_id } } = post
+    newPost.owner = { 
+      nickname: user.nickname,
+      _id
+    }
+    // replace the old post from the current state 
+    const newPosts = [...posts] // copy of posts
+    newPosts[postIndex] = newPost
+    // set the state with the new posts
+    this.setState({
+      posts: newPosts
+    })
+
   }
 
   render() {
@@ -33,7 +57,8 @@ export class PostProvider extends Component {
       <PostContext.Provider value={{
         posts: this.state.posts,
         addPosts: this.addPosts,
-        removePost: this.removePost
+        removePost: this.removePost,
+        editPost: this.editPost
       }}>
         {this.props.children}
       </PostContext.Provider>
