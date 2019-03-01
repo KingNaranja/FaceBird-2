6c53e0ab4b123cfb5096648cb2004ddea45c4348
KingNaranja <kledaa3@gmail.com>
2019-03-01 18:43:20 -0500

    Removed Post & button scss and Added a styled PostWrapper component

--- src/post/components/Post.js
+++ src/post/components/Post.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
-import '../post.scss'
-import '../buttons.scss'
-
 import PostButtons from './Button'
+import styled from 'styled-components'
 
-// takes post object as prop 
+// create styled component wrapper 
+const PostWrapper = styled.div`
+  width: 70vw;
+  border-radius: 2.5vh;
+  .post-header {
+    justify-content: space-around;
+  }
+  .post-body {
+    padding: 2vh;
+  }
+  .button {
+    visibility: hidden;
+    opacity: 0;
+    transition: visibility 0s, opacity 0.5s linear;
+  }
+  :hover {
+    .button {
+      visibility: visible;
+      opacity: 1;
+      >button{
+        margin: 0 10vw 0 10vw ;
+        
+      }
+    }
+  }
+    
+  @media (max-width: 600px){
+    h4,h5,p {
+      font-size: 90%;
+    }
+    .post{
+      width: 90vw;
+    }
+  }
+`
+
+// Post takes post object as prop 
 // and renders Post
 class Post extends Component {
   constructor(props) {
@@ -19,9 +53,8 @@ class Post extends Component {
 
     // set state if the post belongs to the user
     if ( user ) {
-      owner === user._id ? this.setState({isOwner:true}):null
+      owner === user._id ? this.setState({ isOwner:true }):null
     }
-    
     
   }
 
@@ -29,9 +62,9 @@ class Post extends Component {
     const { date, nickname, text, id:owner, user, removePost, editPost } = this.props
 
     return (
-      <div className='post card'>
+      <PostWrapper className='card'>
         <div className="post-header row">
-          <h4>{ nickname}</h4>
+          <h4>{ nickname }</h4>
           <p>{ date }</p>
         </div>
         <div className="post-body">
@@ -39,10 +72,10 @@ class Post extends Component {
           {
             // display auth Buttons if post belongs to user
             this.state.isOwner && 
-            <PostButtons editPost={editPost} removePost={removePost} id={owner} user={user} text={text} />
+            <PostButtons editPost={ editPost } removePost={ removePost } id={ owner } user={ user } text={ text } />
           }
         </div>
-      </div>
+      </PostWrapper>
     )
   }
 }
