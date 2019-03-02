b00e2d475168ae5f3aa23b2ed2b292f323b4c4dd
KingNaranja <kledaa3@gmail.com>
2019-03-01 21:13:07 -0500

    Create styled header component

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,7 +1,67 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
-import './Header.scss'
+const MainHeader = styled.header`
+  align-items: center;
+  border-right: 1px solid #bbb;
+  display: flex;
+  height: 100vh;
+  padding: 0 1em;
+  flex-direction: column;
+
+  a,
+  span {
+    color: #252B35;
+    font-size: 1.2em;
+    margin: 0 15px;
+    text-decoration: none;
+    :hover{
+      color: #4D6A6D;
+    }
+  }
+  
+
+  nav {
+    align-items: flex-start;
+    display: flex;
+    flex-direction:  column;
+    flex-grow: 1;
+    justify-content: space-evenly;
+
+  }
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+    font-size: 1em;
+    height: 20%;
+    width: 160%;
+    border-right: none;
+    border-bottom: 1px solid #bbb;
+    h1 {
+      font-size: 2.5em;
+    }
+  }
+
+  @media (max-width: 450px) {
+    nav {
+      justify-content: space-between;
+      flex-direction: column;
+      width: 100%;
+      padding: 1em;
+      a,
+      span {
+        border-bottom: 1px solid #bbb;
+        border-radius: 1em;
+        // margin-bottom: 5px;
+        text-align: center;
+        width: 100%;
+        margin-left: -1vw;
+      }
+    }
+  }
+
+`
 
 const authenticatedOptions = (
   <React.Fragment>
@@ -25,7 +85,7 @@ const alwaysOptions = (
 )
 
 const Header = ({ user }) => (
-  <header className="main-header">
+  <MainHeader className="main-header">
     <Link to={'/'}><h1>FaceBird</h1></Link>
     <span>The Newest Social Network</span>
     <nav className='nav flex-column justify-content'>
@@ -33,7 +93,7 @@ const Header = ({ user }) => (
       { user ? authenticatedOptions : unauthenticatedOptions }
       { alwaysOptions }
     </nav>
-  </header>
+  </MainHeader>
 )
 
 export default Header
